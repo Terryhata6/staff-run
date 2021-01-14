e9e00a2c1ced08f50c936503b972b8350fa68030
CCapslock <53517856+CCapslock@users.noreply.github.com>
2021-01-14 22:11:31 +0300

    добавил движение персонажа

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public Transform Player;
+	private Vector3 PositionVector;
+	private Vector3 StartPositionVector;
+	private Transform CameraTransform;
+
+	private void Start()
+	{
+		CameraTransform = GetComponent<Transform>();
+		StartPositionVector = Player.position;
+		PositionVector = StartPositionVector - CameraTransform.position;
+	}
+	private void Update()
+	{
+		CameraTransform.position = Player.position - PositionVector;
+	}
+}
