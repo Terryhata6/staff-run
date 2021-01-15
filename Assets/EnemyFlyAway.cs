2d2342b160b55608b3fb1d4da8983f774195342a
CCapslock <53517856+CCapslock@users.noreply.github.com>
2021-01-15 19:13:43 +0300

    добавил скрипт для отлетания врагов + модели и анимации от Костика

---
+++ Assets/EnemyFlyAway.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class EnemyFlyAway : MonoBehaviour
+{
+    public Rigidbody EnemyModelRigidbody;
+    private Vector3 ForceVector;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ForceVector.x = (transform.position.x - other.transform.position.x) * 1000f;
+            ForceVector.z = 1000f;
+
+            Debug.Log("AddForce = " + ForceVector);
+            EnemyModelRigidbody.AddForce(ForceVector);
+            Debug.Log("Hit = other.transform.position : " + other.transform.position + " + transform.position :" + transform.position);
+        }
+    }
+}
