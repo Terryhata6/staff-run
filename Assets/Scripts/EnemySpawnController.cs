28524d3c9a608cb6903386f538695a5a5c738b3e
SempiternalN7 <60327937+SempiternalN7@users.noreply.github.com>
2021-01-15 21:34:58 +0300

    Enemy + NameManager
    Добавил скрипты: Enemy - поведение противника (наблюдение за игроком), скрипт спавна врагов и NameManager, который хранит в себе стринговые значения. Заменил Start на Awake в LevelBuilder, для работы спавна, на данном этапе. Добавил тэги.

---
+++ Assets/Scripts/EnemySpawnController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class EnemySpawnController : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemy;
+    [SerializeField] private GameObject[] _spawnPos; 
+
+    void Start()
+    {
+        SpawnEnemy();
+    }
+
+    private void Update()
+    {
+        if (_spawnPos[0] == null)
+        {
+            SpawnEnemy();
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        _spawnPos = GameObject.FindGameObjectsWithTag(NameManager.SpawnEnemy);
+
+        foreach (GameObject spawn in _spawnPos)
+        {
+            Instantiate(_enemy, spawn.transform.position, spawn.transform.rotation);
+        }
+    }
+}
