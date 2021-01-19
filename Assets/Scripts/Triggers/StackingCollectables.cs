554ceee16986945149e66c0edb52bb05bcba045f
SempiternalN7 <60327937+SempiternalN7@users.noreply.github.com>
2021-01-19 12:05:44 +0300

    Collectables + Spawn
    заменил EnemySpawn на Spawn (Enemy + Collectables), изменил LevelPart1

--- Assets/Scripts/Triggers/StackingCollectables.cs
+++ Assets/Scripts/Triggers/StackingCollectables.cs
@@ -3,10 +3,12 @@
 public class StackingCollectables : MonoBehaviour
 {
     private StickModel _stickModel;
+
     private void Start()
     {
         _stickModel = FindObjectOfType<StickModel>();
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
