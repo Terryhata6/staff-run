d1eaa94a82b2a475dc7ad49a63f5430bb5620f8d
DronCorleone <48313301+DronCorleone@users.noreply.github.com>
2021-01-28 22:15:23 +0300

    Saver & main controller origin

--- Assets/Scripts/Level Generator/SOLevel.cs
+++ Assets/Scripts/Level Generator/SOLevel.cs
@@ -1,12 +1,9 @@
 ﻿using UnityEngine;
 
 
-namespace StaffRun
+[CreateAssetMenu(fileName = "Scene_1", menuName = "Create level", order = 0)]
+public class SOLevel : ScriptableObject
 {
-    [CreateAssetMenu(fileName = "Scene_1", menuName = "Create level", order = 0)]
-    public class SOLevel : ScriptableObject
-    {
-        [Header("Level Parts")]
-        [SerializeField] public LevelPart[] LevelParts;
-    }
+    [Header("Level Parts")]
+    [SerializeField] public LevelPart[] LevelParts;
 }
---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainController : MonoBehaviour
+{
+    [SerializeField] private int _levelNumber;
+
+    private LevelBuilder _levelBuilder;
+    private SaveDataRepo _saveData;
+    private int _currentCoins;
+    private int _coins;
+
+
+    private void Awake()
+    {
+        _saveData = new SaveDataRepo();
+        
+        _levelBuilder = FindObjectOfType<LevelBuilder>();
+
+        _levelNumber = _saveData.LoadInt(SaveKeyManager.KeyLevelNumber);
+        _coins = _saveData.LoadInt(SaveKeyManager.KeyCoins);
+    }
+
+    private void Start()
+    {
+        _levelBuilder.BuildLevel(_levelNumber);
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void PauseGame()
+    {
+        
+    }
+    public void ResumeGame()
+    {
+
+    }
+    public void NextLevel()
+    {
+        _levelNumber++;
+        _coins += 123456; // TODO
+        SaveStats();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void SaveStats()
+    {
+        _saveData.SaveData(_levelNumber, SaveKeyManager.KeyLevelNumber);
+        
+    }
+}
