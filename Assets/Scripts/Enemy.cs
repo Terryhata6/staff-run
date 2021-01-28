794e977d5fc165c28aa1d004724dd454333c55e3
Dron Corleone <kefirchik13@gmail.com>
2021-01-28 17:31:17 +0300

    ragdoll

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,8 +6,12 @@ public class Enemy : MonoBehaviour
     [SerializeField] private Transform _headTransform;
     [SerializeField] private bool _isLooking = true;
     [SerializeField] private GameObject _box;
-    [SerializeField] private GameObject _rigsNormal;
-    [SerializeField] private GameObject _rigsRagdoll;
+    //[SerializeField] private GameObject _rigsNormal;
+    //[SerializeField] private GameObject _rigsRagdoll;
+    [SerializeField] private GameObject _bodyBone;
+    [SerializeField] private Collider _bigCapsule;
+    [SerializeField] private Collider _smallCapsule;
+    [SerializeField] private Collider _cube;
 
     private Transform _player;
     private float _distance;
@@ -27,6 +31,8 @@ public class Enemy : MonoBehaviour
         _playerState = _player.GetComponent<PlayerMovement>();
         _animator = GetComponent<Animator>();
         _collider = GetComponent<CapsuleCollider>();
+
+        ActivateRagdoll(false);
     }
 
     void Update()
@@ -50,8 +56,8 @@ public class Enemy : MonoBehaviour
                 _finalState = true;
                 //_collider.isTrigger = false;
                 //_box.SetActive(false);
-                _rigsNormal.SetActive(false);
-                _rigsRagdoll.SetActive(true);
+                //_rigsNormal.SetActive(false);
+                //_rigsRagdoll.SetActive(true);
                 //_animator.enabled = false;
             }
         }
@@ -60,7 +66,7 @@ public class Enemy : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-              
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -72,12 +78,33 @@ public class Enemy : MonoBehaviour
 
             _animator.enabled = false;
             
-            ForceVector.x = (transform.position.x - other.transform.position.x) * 200f;
-            ForceVector.z = 600f;
+            ForceVector.x = (transform.position.x - other.transform.position.x) * 20f;
+            ForceVector.z = 60f;
             
+            ActivateRagdoll(true);
 
-            EnemyModelRigidbody.AddForce(ForceVector);
+            //EnemyModelRigidbody.AddForce(ForceVector, ForceMode.Impulse);
+            EnemyModelRigidbody.AddForce(Vector3.left * 50, ForceMode.Impulse);
             Destroy(gameObject, 5.0f);
+
         }
+    }
+
+    private void ActivateRagdoll(bool state)
+    {
+        //GetComponent<Rigidbody>().isKinematic = state;
+        //if (state == true)
+        //{
+        //    Destroy(GetComponent<Rigidbody>());
+        //    //_bigCapsule.enabled = false;
+        //    //_smallCapsule.enabled = false;
+        //    _cube.enabled = false;
+        //}
+        var bones = GetComponentsInChildren<Rigidbody>();
+        foreach(var bone in bones)
+        {
+            bone.isKinematic = !state;
+        }
+        _animator.enabled = !state;
     }
 }
