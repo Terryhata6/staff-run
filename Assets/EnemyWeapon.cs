5de7899aa867fc76d7f4b4674cb015d43b48413c
Hypnos <47752595+Terryhata6@users.noreply.github.com>
2021-01-28 23:38:09 +0300

    Commits

---
+++ Assets/EnemyWeapon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class EnemyWeapon : MonoBehaviour
+{
+    private MeshCollider _collider;
+    private Rigidbody _rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _collider = GetComponentInChildren<MeshCollider>();
+        _collider.enabled = false;
+        _rigidbody.isKinematic = true;
+        _rigidbody.useGravity = false;
+    }
+
+    // Update is called once per frame
+    
+
+    public void OnEnemyDeath()
+    {
+        transform.parent = null;
+        _rigidbody.isKinematic = false;
+        _rigidbody.useGravity = true;
+        _collider.enabled = true;
+        _collider.convex = true;
+       
+    }
+}
