40657db860b534152fb019beae9efb3ce81a5ab6
SempiternalN7 <60327937+SempiternalN7@users.noreply.github.com>
2021-01-28 22:13:22 +0300

    Added Coin
    Добавил Монетки (Спавн + Manager etc.)

--- Assets/Scripts/NameManager.cs
+++ Assets/Scripts/NameManager.cs
@@ -2,6 +2,7 @@
 
 public static class NameManager
 {
+    public const string SpawnCoins = "SpawnCoins";
     public const string SpawnEnemy = "SpawnEnemy";
     public const string SpawnCollectables = "SpawnCollectables";
     public const string Player = "Player";
