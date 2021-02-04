0cccc8cb163cf85365e88482440d85fed04abb02
CCapslock <53517856+CCapslock@users.noreply.github.com>
2021-02-04 11:40:25 +0300

    игрок поворачивается

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -7,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 	public float MovingSpeed;
 	public float MovingUpSpeed;
 	public float MovingDownSpeed;
+	public float RotationForce;
 
 	[SerializeField] private InputController _inputController;
 	[SerializeField] private StickModel _stickModel;
@@ -15,12 +15,15 @@ public class PlayerMovement : MonoBehaviour
 	[SerializeField] public CharacterState _currentState;
 	[SerializeField] private Vector3 _delay;
 	[SerializeField] private Vector3 _startPosition;
+	[SerializeField] private Vector3 _startTouchPosition;
 	[SerializeField] private Vector3 _movingVector;
+	[SerializeField] private Vector3 _rotationVector;
 	[SerializeField] private Vector3 _screenWall;
 	[SerializeField] private Vector2 _minScreenPosition, _maxScreenPosition;
 	[SerializeField] private bool _delayCounted;
 	[SerializeField] private bool _isRunning;
 	[SerializeField] private float _sliderSensetivity = 3.0f;
+	[SerializeField] private float _sideDelay;
 	[SerializeField] private Animator _animator;
 	[SerializeField] private StickModel _staff;
 
@@ -39,6 +42,7 @@ public class PlayerMovement : MonoBehaviour
 		_minScreenPosition = Camera.main.ViewportToWorldPoint(new Vector2(0, 0)) * 3f;
 		_maxScreenPosition = Camera.main.ViewportToWorldPoint(new Vector2(1, 1)) * 3f;
 		_movingVector = new Vector3();
+		_rotationVector = new Vector3();
 		_screenWall = new Vector3(Mathf.Clamp(transform.position.x, _minScreenPosition.x, _maxScreenPosition.x), Mathf.Clamp(transform.position.y, _minScreenPosition.y, _maxScreenPosition.y), transform.position.z);
 		_playerRigidbody = GetComponent<Rigidbody>();
 		_isRunning = true;
@@ -50,99 +54,101 @@ public class PlayerMovement : MonoBehaviour
 		switch (_currentState)
 		{
 			case CharacterState.Run:
-                {
-					
+				{
+
 					_animator.SetBool(NameManager.RunState, true);
 					_animator.SetBool(NameManager.FlyState, false);
 					OnRunMovement();
-                    break;
-                }
-            case CharacterState.Fly:
-                {					
+					break;
+				}
+			case CharacterState.Fly:
+				{
 					_animator.SetBool(NameManager.RunState, false);
 					_animator.SetBool(NameManager.FlyState, true);
 					OnFlyMovement();
-                    break;
-                }
+					break;
+				}
 			case CharacterState.Final:
 				{
-					
+
 					_animator.SetBool(NameManager.RunState, false);
 					_animator.SetBool(NameManager.FlyState, false);
 					_animator.SetBool(NameManager.FinalState, true);
 					break;
 				}
-            default: _animator.SetBool(NameManager.RunState, true); break;
-		}	
-		
-		if(_playerTransform.position.y <= -5)
+			default: _animator.SetBool(NameManager.RunState, true); break;
+		}
+
+		if (_playerTransform.position.y <= -5)
 		{
 			Debug.Log("Game Over");
 		}
 		//ограничение экрана
 		/*_screenWall.x = Mathf.Clamp(transform.position.x, _minScreenPosition.x, _maxScreenPosition.x);
 		_screenWall.y = Mathf.Clamp(transform.position.y, _minScreenPosition.y, _maxScreenPosition.y);
-		transform.position = _screenWall;*/		
+		transform.position = _screenWall;*/
 	}
 
-    
 
-    private void OnFlyMovement()
-    {
-        _movingVector = _playerTransform.position;
-        _movingVector.z += MovingSpeed;
+
+	private void OnFlyMovement()
+	{
+		_movingVector = _playerTransform.position;
+		_movingVector.z += MovingSpeed;
 		if (_inputController.DragingStarted && _stickModel.CanFlyUp)
-        {
-            _movingVector.y += MovingUpSpeed;
-        }		
+		{
+			_movingVector.y += MovingUpSpeed;
+		}
 		else
 		{
 			_movingVector.y -= MovingDownSpeed;
 		}
-        _playerTransform.position = _movingVector;
-		
-    }
-    private void OnRunMovement()
-    {
-        _movingVector = _playerTransform.position;
-        _movingVector.z += MovingSpeed;
-        if (_inputController.DragingStarted)
-        {
-            if (!_delayCounted)
-            {
-                _delay = _inputController.TouchPosition;
-                _startPosition = _playerTransform.position;
-                _delayCounted = true;
-            }
-            _movingVector.x = _startPosition.x + (_inputController.TouchPosition.x - _delay.x) * _sliderSensetivity;			
-            _playerTransform.position = _movingVector;
-        }
-        else
-        {
-            _delayCounted = false;
-        }
+		_playerTransform.position = _movingVector;
 
-		if (_inputController.TouchPosition.x - _delay.x > 0)
+	}
+	private void OnRunMovement()
+	{
+		_movingVector = _playerTransform.position;
+		_movingVector.z += MovingSpeed;
+		if (_inputController.DragingStarted)
 		{
-			_animator.SetBool("RightRun", true);
-			_animator.SetBool("LeftRun", false);
+			if (!_delayCounted)
+			{
+				_delay = _inputController.TouchPosition;
+				_startTouchPosition = _inputController.TouchPosition;
+				_startPosition = _playerTransform.position;
+				_delayCounted = true;
+			}
+			_movingVector.x = _startPosition.x + (_inputController.TouchPosition.x - _delay.x) * _sliderSensetivity;
+			_playerTransform.position = _movingVector;
 		}
-		else if (_inputController.TouchPosition.x - _delay.x < 0)
+		else
 		{
-			_animator.SetBool("RightRun", false);
-			_animator.SetBool("LeftRun", true);
+			_delayCounted = false;
 		}
-		else if(!_inputController.DragingStarted)
-		{
-			_animator.SetBool("RightRun", false);
-			_animator.SetBool("LeftRun", false);
-		}
-        _playerTransform.position = _movingVector;
-    }
+		_sideDelay = _startTouchPosition.x - _inputController.TouchPosition.x;
 
-	
+		if (_sideDelay > 0)
+		{
+			_startTouchPosition.x -= 0.04f;
+		}
+		else if (_sideDelay < 0)
+		{
+			_startTouchPosition.x += 0.04f;
+		}
 
-    public void ChangePlayerState(CharacterState state)
+		if (_sideDelay > 0.05f || _sideDelay < -0.05f)
+		{
+			_rotationVector = _playerTransform.rotation.eulerAngles;
+			_rotationVector.y = _sideDelay * RotationForce * -1f;
+			_playerTransform.rotation = Quaternion.Euler(_rotationVector);
+		}
+		_playerTransform.position = _movingVector;
+	}
+
+
+
+	public void ChangePlayerState(CharacterState state)
 	{
 		Debug.Log("ChangePalyerState");
 
@@ -164,7 +170,7 @@ public class PlayerMovement : MonoBehaviour
 		}
 		else if (state == CharacterState.Final && _currentState != state)
 		{
-			
+
 			_currentState = state;
 			_isRunning = false;
 		}
